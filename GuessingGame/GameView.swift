772921d03b57ff53ddfbb8f1abbb847089188d34
Elaine Fang <efang@lcs.on.ca>
2025-01-16 14:41:42 -0500

    Re-organized project and created a GameView file.

---
+++ GuessingGame/GameView.swift
@@ -0,0 +1,19 @@
+//
+//  GameView.swift
+//  GuessingGame
+//
+//  Created by Fang Elaine on 2025/1/16.
+//
+
+import SwiftUI
+
+struct GameView : View {
+    var body: some View{
+        Text("Hello World")
+    }
+}
+
+#Preview{
+    
+    GameView()
+}
