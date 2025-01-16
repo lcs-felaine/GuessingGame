1e8c2a729054eb65ba47b0663f544a72dbfd4e15
Elaine Fang <efang@lcs.on.ca>
2025-01-16 14:34:11 -0500

    Initial Commit

---
+++ GuessingGame/GuessingGameApp.swift
@@ -0,0 +1,17 @@
+//
+//  GuessingGameApp.swift
+//  GuessingGame
+//
+//  Created by Fang Elaine on 2025/1/16.
+//
+
+import SwiftUI
+
+@main
+struct GuessingGameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
